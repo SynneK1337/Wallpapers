da3074e06a4be39b6d5d264ee74ba6d23a885508
SynneK1337 <276786@pw.edu.pl>
2018-02-07 22:25:26 +0100

    duplicates removing tool fixed

--- delete_clones.py
+++ delete_clones.py
@@ -4,15 +4,18 @@ import os
 path = os.getcwd()
 filenames = os.listdir(os.getcwd())
 hashes = []
+duplicates = []
 def md5(fname):
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
            hash_md5.update(chunk)
         if hash_md5.hexdigest() in hashes:
-            print(fname, " Removed.")
-            os.remove(fname)
+            duplicates.append(fname)
         hashes.append(hash_md5.hexdigest())
 
 for filename in filenames:
     md5(filename)
+
+for x in duplicates:
+    os.remove(x)
